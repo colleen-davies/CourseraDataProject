d740640275f9eff0791c32eff5b12028581bd93b
colleen-davies <colleendavies@icloud.com>
2021-02-23 18:06:19 -0700

    Initial script commit

---
+++ run_analysis.R
@@ -0,0 +1,109 @@
+## ((1)) MERGE DATA SETS
+
+# Load in the data
+# Train:
+X_train <- read.table("./UCI_HAR_Dataset/train/X_train.txt")
+y_train <- read.table("./UCI_HAR_Dataset/train/y_train.txt")
+subject_train <- read.table("./UCI_HAR_Dataset/train/subject_train.txt")
+# Test:
+X_test <- read.table("./UCI_HAR_Dataset/test/X_test.txt")
+y_test <- read.table("./UCI_HAR_Dataset/test/y_test.txt")
+subject_test <- read.table("./UCI_HAR_Dataset/test/subject_test.txt")
+
+# Make them into full data frames
+train_df <- cbind(subject_train, y_train, X_train)
+test_df <- cbind(subject_test, y_test, X_test)
+
+# Quick name adjustment for three "V1"s
+original_var_names <- names(train_df)
+new_var_names <- c("subject", "classlabel", original_var_names[3:563])
+colnames(train_df) <- new_var_names
+colnames(test_df) <- new_var_names
+
+# Merge
+full_df <- rbind(train_df, test_df)
+
+## ((2)) EXTRACT MEASUREMENTS
+
+# Read in feature names
+features <- read.table("./UCI_HAR_Dataset/features.txt")
+featurenames <- features[[2]]
+
+# Search for "mean()" among feature names
+mean_indices <- grep("mean()", featurenames, fixed=TRUE) 
+#    fixed = TRUE means only strings including "mean()" exactly will be included
+#    and not "meanFreq()"
+
+# Search for "std()" among feature names
+std_indices <- grep("std()", featurenames, fixed=TRUE) 
+
+# Choose which columns to keep
+feature_indices <- c(mean_indices, std_indices)
+V_indices <- feature_indices + 2 # added y and subject to left of test
+full_indices <- c(1,2, V_indices)
+full_indices_sort <- sort(full_indices)
+
+# Extract measurements
+measure_df <- full_df[,full_indices_sort]
+
+## ((3)) NAME ACTIVITIES
+
+# Make new activity labels
+activity_labels_txt <- read.table("./UCI_HAR_Dataset/activity_labels.txt")
+activity_labels_orig <- activity_labels_txt[,2]
+activity_labels_lc <- tolower(activity_labels_orig)
+activity_labels <- sub("_", " ", activity_labels_lc)
+
+# Extract the original class labels
+data_class_labels <- measure_df[,2] # numeric factors
+
+# Convert the numeric to character factors
+data_activities <- sapply(data_class_labels, function(x) activity_labels[x]) 
+
+# Replace the current "classlabel" column with "activities" factor
+measure_df$classlabel <- as.factor(data_activities)
+
+## ((4)) LABEL DATA SET
+
+# slightly better labels
+better_labels <- c("subject", "activity", featurenames[feature_indices])
+
+# even better labels
+# replace initial "t" with "time"
+featnames1 <- sub("^t", "time", featurenames[feature_indices],)
+# replace initial "f" with "freq"
+featnames2 <- sub("^f", "freq", featnames1,)
+# replace "Acc" with "Accel"
+featnames3 <- sub("Acc", "Accel", featnames2,)
+# replace "-" with ""
+featnames4 <- gsub("-", "", featnames3,)
+# replace "()" with ""
+featnames5 <- sub("\\()", "", featnames4,)
+# fix BodyBody
+featnames6 <- sub("BodyBody", "Body", featnames5,)
+
+# final labels, with all columns
+final_labels <- c("subject", "activity", featnames6)
+
+# implement the final labels
+colnames(measure_df) <- final_labels
+
+## ((5)) CREATE INDEPENDENT TIDY DATA SET 
+
+# install dplyr if necessary
+pckglist <- rownames(installed.packages())
+if(!("dplyr" %in% pckglist)) {install.packages("dplyr")}
+library(dplyr)
+
+# group by subject and activity and summarize variables
+avg_df_init <- tibble::as_tibble(measure_df) # make a data frame table
+avg_df <- avg_df_init %>%
+    group_by(subject, activity) %>%
+    summarize(across(everything(), mean, .names = "avg{.col}"), 
+              .groups = "drop")
+
+# write the data to an independent file
+write.table(avg_df, file = "avgvar.txt", row.names = FALSE)
+
+# to read this data in directly, use:
+# avgvardf <- read.table("avgvar.txt", header = TRUE)
